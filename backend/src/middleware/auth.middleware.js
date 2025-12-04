86c4e774285fbd4bd32e97967c9cd0789d7f2ff3
abhijithwinddaa <abhijithyadav786@gmail.com>
2025-12-04 15:58:29 +0530

    Initialize full-stack application with user authentication and signup functionality.

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,19 +5,19 @@ import User from '../models/User.js';
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies?.jwt
-        if (!token) return res.status(401).json({message: "Unauthorized access. No token provided."})
+        if (!token) return res.status(401).json({ message: "Unauthorized access. No token provided." })
 
-        const decodec = jwt.verify(token, ENV.JWT_SECRET);
-        if (!decodec) return res.status(401).json({message: "Unauthorized access. Invalid token."}) 
+        const decoded = jwt.verify(token, ENV.JWT_SECRET);
+        if (!decoded) return res.status(401).json({ message: "Unauthorized access. Invalid token." })
 
-        const user = await User.findById(decodec.userId).select("-password");
-        if (!user) return res.status(401).json({message: "User not found."})   
-        
-        req.user = user;    
-        next();    
+        const user = await User.findById(decoded.userId).select("-password");
+        if (!user) return res.status(401).json({ message: "User not found." })
+
+        req.user = user;
+        next();
     } catch (error) {
         console.error("Error in protectRoute middleware:", error);
-        res.status(401).json({message: "internal server error"})
+        res.status(401).json({ message: "internal server error" })
     }
 }
 
